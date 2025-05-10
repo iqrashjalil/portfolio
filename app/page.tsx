3cbc1e948b9eb50d64ecd8e8762f8378f6d1c6b0
Iqrash Jalil <iqrashjalil@gmail.com>
2025-05-11 02:05:23 +0500

    Changes the cv

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   };
   const handleDownload = () => {
     const link = document.createElement("a");
-    link.href = "/iqrashjalilcv.pdf";
+    link.href = "/iqrashjalil.pdf";
     link.download = "cv.pdf";
     document.body.appendChild(link);
     link.click();
